dd5809cd0e49c944c945eef35e104e0abae86ff2
Danish <dan71ish@gmail.com>
2023-11-14 22:57:20 +0530

    product page some tweaks

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -4,7 +4,7 @@ import Products from "./Products";
 const ProductList = () => {
   const { data } = useProductContext();
   return (
-    <div className="flex flex-wrap items-center justify-center gap-4 py-2">
+    <div className="flex flex-wrap items-center justify-center gap-4 py-2 my-4">
       {data.map((currElem) => (
         <Products key={currElem.id} {...currElem} />
       ))}
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,36 +1,20 @@
+import { NavLink } from "react-router-dom";
+
 const Products = (currElem) => {
-  console.log("djlks");
   const { title, price, image, category } = currElem;
   return (
-    //   <div className="w-44">
-    //     <figure>
-    //       <img src={image} className="w-28 mx-auto" alt={title} />
-    //       <figcaption className="text-gray-400">{category}</figcaption>
-    //     </figure>
-    //     <div className="card-data">
-    //       <h3>{title}</h3>
-    //       <p className="">${price}</p>
-    //     </div>
-    //   </div>
-
-    <div className="mx-auto mt-11 w-80 transform overflow-hidden rounded-lg bg-white dark:bg-slate-800 shadow-md duration-300 hover:scale-105 hover:shadow-lg">
+    <div className="mx-auto mt-11 w-36 md:w-80 transform overflow-hidden rounded-lg bg-white shadow-md duration-300 hover:scale-105 hover:shadow-lg">
       <img
         className="h-48 mx-auto object-cover object-center"
         src={image}
         alt={title}
       />
       <div className="p-4">
-        <h2 className="mb-2 text-lg font-medium dark:text-white text-gray-900">
-          {title}
-        </h2>
-        <p className="mb-2 text-base dark:text-gray-300 text-gray-700">
-          {category}{" "}
-        </p>
+        <h2 className="mb-2 text-lg font-medium text-gray-900">{title}</h2>
+        <p className="mb-2 text-base text-gray-700">{category} </p>
         <div className="flex items-center">
-          <p className="mr-2 text-lg font-semibold text-gray-900 dark:text-white">
-            ${price}
-          </p>
-          <p className="text-base  font-medium text-gray-500 line-through dark:text-gray-300">
+          <p className="mr-2 text-lg font-semibold text-gray-900">${price}</p>
+          <p className="text-base  font-medium text-gray-500 line-through">
             {(price % 3).toFixed(2)}
           </p>
           <p className="ml-auto text-base font-medium text-green-500">
