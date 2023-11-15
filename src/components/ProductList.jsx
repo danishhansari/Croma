a93b95d342b515aacbba52d91579ced9cb2fbc0d
Danish <dan71ish@gmail.com>
2023-11-15 21:19:44 +0530

    product page

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -5,7 +5,7 @@ import Products from "./Products";
 const ProductList = () => {
   const { data } = useProductContext();
   return (
-    <div className="flex flex-wrap items-center justify-center gap-4 py-2 my-4">
+    <div className="flex flex-wrap items-center justify-center gap-2 py-2 my-4">
       {data.map((currElem) => (
         <Products key={currElem.id} {...currElem} />
       ))}
--- src/components/SingleProduct.jsx
+++ src/components/SingleProduct.jsx
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import axios from "axios";
 
 const API = "https://fakestoreapi.com/products";
@@ -19,13 +20,29 @@ const SingleProduct = () => {
   useEffect(() => {
     getProductDetails();
   }, [id]);
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [id]);
 
   return (
     <div>
+      <button>
+        <NavLink to="/products">Products</NavLink>
+      </button>
       {product ? (
-        <div>
-          <h2>{product.title}</h2>
-          <p>{product.description}</p>
+        <div className="flex flex-col md:flex-row  md:items-center justify-between max-w-[1200px] mx-auto py-6">
+          <img className="w-48 mx-auto" src={product.image} />
+          <div className="md:w-2/3 p-2">
+            <h2 className="text-center text-xl">{product.title}</h2>
+            <p className="text-gray-800">{product.description}</p>
+            <div className="flex items-center px-2 justify-between">
+              <p className="text-2xl mt-4 text-red-400">${product.price}</p>
+              <p className="ml-auto text-base font-medium text-green-500">
+                30% off
+              </p>
+            </div>
+            <p className="mt-4">{product.rating.count} reviews  </p>
+          </div>
         </div>
       ) : (
         "Loading..."
